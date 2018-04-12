7ac65d241c92fa20824e65bf9016d70877d59aa6
braulioRam <braulio.ramirez@justia.com>
2018-04-11 23:07:16 -0500

    More listings.

--- Stores.php
+++ Stores.php
@@ -12,11 +12,17 @@ Class Stores {
                     'guitar-amplifiers' => '/b/ref=lp_11091801_ln_3_0?node=486410011&ie=UTF8&qid=1522449197'
                 ]
             ],
-            'amazonmx' => [
+            'amazon-mx' => [
                 'parser' => 'braulioRam\dealsNotifier\Amazon\AmazonParser',
                 'domain' => 'https://www.amazon.com.mx',
                 'listings' => [
-                    'guitar-amplifiers' => '/b?ie=UTF8&node=14652402011'
+                    'electric-guitars' => '/b?ie=UTF8&node=14652407011',
+                    'guitar-amplifiers' => '/b?ie=UTF8&node=14652402011',
+                    'guitar-effects' => '/b?ie=UTF8&node=14652403011',
+                    'cool-headphones' => '/Aud%C3%ADfonos-Marca-6-seleccionados/s?ie=UTF8&page=1&rh=n%3A9687308011%2Cp_89%3AAKG%7CAudio-Technica%7CBose%7CSennheiser%7CSony%7Cbeyerdynamic',
+                    'baby-monitors' => '/b?ie=UTF8&node=9841137011',
+                    'baby-car-mirrors' => '/baby-car-mirror/s?ie=UTF8&page=1&rh=i%3Aaps%2Ck%3Ababy%20car%20mirror',
+                    'instax-films' => '/instax-Rollos-Accesorios/s?ie=UTF8&page=1&rh=n%3A9687793011%2Ck%3Ainstax'
                 ]
             ]
         ];
